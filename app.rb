b40e5debee5137464f9e371d9ac04cf994bde430
antonio jurado <iberico.cortador@gmail.com>
2022-08-19 22:35:28 +0900

    added views

--- app.rb
+++ app.rb
@@ -8,6 +8,23 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+# [...]
+
 get "/" do
-  "Hello world!"
+  erb :index
+end
+
+# [...]
+
+get "/about" do
+  erb :about
+end
+
+# [...]
+
+# [...]
+
+get "/team/:username" do
+  puts params[:username]
+  "The username is #{params[:username]}"
 end
